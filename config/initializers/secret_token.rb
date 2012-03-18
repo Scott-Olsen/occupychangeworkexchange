d109388a12f676bf61f5d0782371199481307576
Scott Olsen <solsen230@gmail.com>
2012-03-17 23:09:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ocwe::Application.config.secret_token = 'f8cc4e72c07842c57d0239508c5d20589c5584b956a0b3bb3290be8c3d8748653e95df35343bdd7051db3296ae8a00e99c700603b5b5d6d40f1ea9d177865450'
